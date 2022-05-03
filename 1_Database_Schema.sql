873b8edddbe85000660d2763e96f8d250bb19211
MrAKowalski <56323311+MrAKowalski@users.noreply.github.com>
2022-05-03 11:30:01 +0200

    Cleaning_Code
    Additional comments and format changes

--- 1_Database_Schema.sql
+++ 1_Database_Schema.sql
@@ -4,12 +4,13 @@ DATABASE SCHEMA
 In this script I will show how to create sample database from scratch 
 and how to modify table elements at this stage. All conception base on the
 ER diagram/db model -> it was prepared on the website app.diagrams.net 
+
 */ 
 
 CREATE DATABASE Cinemas
 
 USE Cinemas
-
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Cinema (
 
 	Id INT IDENTITY PRIMARY KEY,
@@ -21,6 +22,7 @@ CREATE TABLE Cinema (
 	Close_Date DATE					-- cinema can be still open, NULL val allowed
 );
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Cinema_Room (
 	Id INT IDENTITY PRIMARY KEY,
 	Name VARCHAR(50) NOT NULL,
@@ -29,6 +31,7 @@ CREATE TABLE Cinema_Room (
 	Id_Cinema INT FOREIGN KEY REFERENCES Cinema(Id)
 );
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Cinema_Seats (
 	Id INT IDENTITY PRIMARY KEY,
 	No_Seats VARCHAR(50),
@@ -36,6 +39,7 @@ CREATE TABLE Cinema_Seats (
 	Id_Cinema INT FOREIGN KEY REFERENCES Cinema_Room(Id)
 );
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Movie (
 	Id INT IDENTITY PRIMARY KEY,
 	Title VARCHAR(300) NOT NULL,
@@ -51,17 +55,44 @@ CREATE TABLE Movie (
 	License_Cost MONEY					-- alternatively REAL
 );
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE TABLE Show (
 	Id INT IDENTITY PRIMARY KEY,
-	Title VARCHAR(200) NOT NULL,
+	--Title VARCHAR(200) NOT NULL,
 	Show_Date DATE NOT NULL,
 	Show_Hour TIME(0) NOT NULL,
 	Ticket_Price MONEY NOT NULL,		-- there is no free shows in cinema :)
-	Id_Movie INT FOREIGN KEY REFERENCES Movie(Id), 
-	Id_Cinema_Room INT FOREIGN KEY REFERENCES Cinema_Room(Id)
+	Id_Movie INT FOREIGN KEY REFERENCES Movie(Id) NOT NULL, 
+	Id_Cinema_Room INT FOREIGN KEY REFERENCES Cinema_Room(Id) NOT NULL
 );
 
--- sample changes: drop colum & changing data type:
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE Client (
+	Id INT IDENTITY PRIMARY KEY,
+	Name VARCHAR(20) NOT NULL,
+	Last_Name VARCHAR(50) NOT NULL,
+	Client_Log VARCHAR(20) NOT NULL,
+	Email VARCHAR(20),
+	Account_Creation_Date DATETIME NOT NULL
+);
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE TABLE Reservation (
+	Id INT IDENTITY PRIMARY KEY,
+	Reservation_Date DATE NOT NULL,
+	Reservation_hour TIME(0),
+	Paymeny_Check CHAR(1),						-- paid y, unpaid n
+	Payment_Type VARCHAR(10),					-- internet, mobile, cinema ect.
+	Id_Show INT FOREIGN KEY REFERENCES Show(Id) NOT NULL,
+	Id_Cinema_Room INT FOREIGN KEY REFERENCES Cinema_Room(Id) NOT NULL,
+	Id_Client INT FOREIGN KEY REFERENCES Client(Id)
+);
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+SAMPLE CHANGES:
+
+--- drop colum & changing data type:
 
 ALTER TABLE Show DROP COLUMN Title;			-- title in Show table is redundant // there is the same col in Movie table
 
@@ -71,17 +102,12 @@ ALTER COLUMN Id_Movie INT NOT NULL;			-- I forgot to add NOT NULL statment
 ALTER TABLE Show
 ALTER COLUMN Id_Cinema_Room INT NOT NULL;	-- same as above
 
----
 
-CREATE TABLE Client (
-	Name VARCHAR(20) NOT NULL,
-	Last_Name VARCHAR(50) NOT NULL,
-	Client_Log VARCHAR(20) NOT NULL,
-	Email VARCHAR(20),
-	Account_Creation_Date DATETIME NOT NULL
-)
+EXEC sp_fkeys 'Show'  --- check fk keys connected with table
+USE Cinemas
+DROP TABLE Show;
 
--- Adding new column:
+--- adding new column:
 
 ALTER TABLE Client
 ADD Id INT IDENTITY PRIMARY KEY;
@@ -89,22 +115,10 @@ ADD Id INT IDENTITY PRIMARY KEY;
 ALTER TABLE Client
 ADD Birth_Date DATE NOT NULL;
 
---
+--- rename of columns:
 
-CREATE TABLE Reservation (
-	Id INT IDENTITY PRIMARY KEY,
-	Reservation_Date DATE NOT NULL,
-	Reservation_hour TIME(0),
-	Paymeny_Check CHAR(1),						-- paid y, unpaid n
-	Payment_Type VARCHAR(10),					-- internet, mobile, cinema ect.
-	id_seansu INT FOREIGN KEY REFERENCES Show(Id) NOT NULL,
-	id_miejsca INT FOREIGN KEY REFERENCES Cinema_Room(Id) NOT NULL,
-	id_klienta INT FOREIGN KEY REFERENCES Client(Id)
-);
-
--- Rename of columns:
+-- Attention, the reletionship ID still will be based on previous col name!
 
 EXEC sp_rename 'Reservation.id_seansu', 'Id_Show', 'COLUMN';
 EXEC sp_rename 'Reservation.id_miejsca', 'Id_Cinema_Room', 'COLUMN';
 EXEC sp_rename 'Reservation.id_klienta', 'Id_Client', 'COLUMN';
-
---
+++ 2_Insterts.sql
@@ -0,0 +1,109 @@
+﻿/*
+INSERTS:
+
+Intro table inserts, values wil be changes with procedures later.
+
+*/
+
+USE Cinemas
+GO
+
+-- ################################################### CINEMA #####################################################################################
+
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Blue Diamond', 'Wroclove', 'Sucha 12', '00-001', '20200601', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Jadeite', 'Warsaw', 'Ostrobramska 74', '00-002', '20210421', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Emerald', 'Cracov', 'Podgorska 43', '00-003', '19010801', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Blac Opal', 'Gdansk', 'Schubertta 120B', '00-004', '20190812', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Red Beryl', 'Poznan', 'Boleslawa 72', '00-001', '20170507', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Bixbite', 'Lodz', 'Drewnowska 57', '00-001', '20150915', NULL);
+INSERT INTO Cinema(Name, City, Street, Post_Code, Open_Date, Close_Date) VALUES('Coal', 'Sopot', 'Nadmorska 101', '00-007', '20190818', NULL);
+
+/*
+-- Sample table updates:
+
+UPDATE Cinema
+SET Open_Date = '20181027',
+	Close_Date = '20210421'		
+	WHERE Name = 'Jadeite'
+
+UPDATE Cinema SET Post_Code = '00-005' WHERE Name = 'Red Beryl';
+UPDATE Cinema SET Post_Code = '00-006' WHERE Name = 'Bixbite'
+
+DELETE FROM Cinema WHERE Name = 'Bixbite' AND City = 'Sopot';
+DELETE FROM Cinema WHERE Id BETWEEN 8 AND 14; -- table was overwrite twice, here are sample code how to delete selected rows.
+*/
+
+-- ############################################ CINEMA_ROOM TABLE ##################################################################################
+
+-- SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'Cinema_Room'
+
+BULK INSERT Cinema_Room
+	FROM   'C:\Users\Ela\Desktop\SQL_Crash_Course\Project\Cinema_Rooms.csv'
+	WITH
+	(
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n', 
+		CODEPAGE =  'ACP' -- | 'OEM' | 'RAW' | 'code_page'  
+	)
+
+-- ################################################# CINEMA_SEATS ###########################################################################################
+
+BULK INSERT Cinema_Seats
+	FROM   'C:\Users\Ela\Desktop\SQL_Crash_Course\Project\Cinema_Seats.csv'
+	WITH
+	(	
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n', 
+		CODEPAGE =  'ACP' -- | 'OEM' | 'RAW' | 'code_page'  
+	)
+
+-- ################################################# MOVIE ###########################################################################################
+
+ BULK INSERT Movie
+	FROM   'C:\Users\Ela\Desktop\SQL_Crash_Course\Project\Movie.csv'
+	WITH
+	(	
+		FIELDTERMINATOR = '|',
+		ROWTERMINATOR = '\n',
+		CODEPAGE =  '65001' -- | 'OEM' | 'RAW' | 'code_page'  --- UTF 8 65001
+	)
+
+-- ################################################# SHOW ###########################################################################################
+
+BULK INSERT Show
+	FROM   'C:\Users\Ela\Desktop\SQL_Crash_Course\Project\Shows.csv'
+	WITH
+	(
+		FIELDTERMINATOR = ';',
+		ROWTERMINATOR = '\n', 
+		CODEPAGE =  'ACP' -- | 'OEM' | 'RAW' | 'code_page'  
+	)
+
+-- ################################################# CHEKING QUERIES ###########################################################################################
+/*
+
+-- Checking if there are duplicated values:
+WITH base2 AS
+(SELECT Id, Id_Cinema_Room, Show_Date, COUNT(*) OVER (PARTITION BY Id_Cinema_Room, Show_Date) AS Cnt_Cinema_Room
+FROM Show
+)
+
+SELECT *
+FROM base2
+WHERE Cnt_Cinema_Room > 1;
+
+---- 
+-- Checking unused id:
+
+WITH base3 
+AS
+(SELECT DISTINCT(Id_Cinema_Room) as Dist_Id_Cin FROM Show)
+
+SELECT Id FROM Cinema_Room AS Cr
+WHERE Id not in (SELECT Dist_Id_Cin FROM base3)
+
+SELECT * FROM Show
+
+USE Cinemas
+DROP DATABASE Cinemas
+*/
